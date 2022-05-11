163264d991a7ae59cdcec2f5edbf9777df0e71fb
ashcolor <ashcolor27@gmail.com>
2022-05-11 19:38:25 +0900

    add: vue init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ashcolor.github.io
+
+## URL
+
+[https://ashcolor.github.io/](https://ashcolor.github.io/)
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn dev
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import Inspector from "vite-plugin-vue-inspector";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "./",
+    plugins: [
+        vue(),
+        AutoImport({
+            include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+            imports: [
+                "vue",
+                {
+                    "@vueuse/core": ["useMouse", ["useFetch", "useMyFetch"]],
+                    axios: [["default", "axios"]],
+                    pinia: ["storeToRefs"],
+                },
+            ],
+            dts: "./src/auto-imports.d.ts",
+        }),
+        Components({
+            dts: "./src/components.d.ts",
+        }),
+        Inspector(),
+    ],
+});
