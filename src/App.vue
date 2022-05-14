2009c0155f440f3ffd5481b1dc56249ed8c7ccfd
ashcolor <ashcolor27@gmail.com>
2022-05-14 11:07:32 +0900

    add: App.vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div class="flex">
+        <ProfileContainer />
+        <MainContainer />
+    </div>
+    <MyFooter />
+</template>
+
+<style scoped></style>
