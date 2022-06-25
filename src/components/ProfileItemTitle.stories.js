a41f544889d1230eccebffb0a7b61ebe0f7a3224
ashcolor <ashcolor27@gmail.com>
2022-06-25 10:31:06 +0900

    chg: atmic designをやめる

---
+++ src/components/ProfileItemTitle.stories.js
@@ -0,0 +1,25 @@
+import ProfileItemTitle from "./ProfileItemTitle.vue";
+
+export default {
+    component: ProfileItemTitle,
+    argTypes: {
+        slot: {
+            control: "string",
+            description: "タイトル",
+            defaultValue: "タイトル",
+        },
+    },
+};
+
+const Template = (args) => ({
+    components: { ProfileItemTitle },
+    setup() {
+        return { args };
+    },
+    template: "<ProfileItemTitle> {{ args.slot }} </ProfileItemTitle>",
+});
+
+export const Normal = Template.bind({});
+Normal.args = {
+    slot: "タイトル",
+};
