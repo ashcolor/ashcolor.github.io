61906871e2ad8b84481b5d3a98003a6a9f8057d1
ashcolor <ashcolor27@gmail.com>
2022-10-17 22:03:23 +0900

    chg: storybookでvueプラグインを使用する

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
+const AutoImport = require("unplugin-auto-import/vite");
+const Components = require("unplugin-vue-components/vite");
+const path = require("path");
+const { mergeConfig } = require("vite");
+
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
     addons: [
@@ -11,5 +16,31 @@ module.exports = {
     },
     features: {
         storyStoreV7: true,
+    },
+    viteFinal: async (config) => {
+        return mergeConfig(config, {
+            resolve: {
+                alias: {
+                    "~": path.resolve(__dirname, "../src"),
+                },
+            },
+            plugins: [
+                AutoImport({
+                    include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+                    imports: [
+                        "vue",
+                        {
+                            "@vueuse/core": ["useMouse", ["useFetch", "useMyFetch"]],
+                            axios: [["default", "axios"]],
+                            pinia: ["storeToRefs"],
+                        },
+                    ],
+                    dts: "./src/auto-imports.d.ts",
+                }),
+                Components({
+                    dts: "./src/components.d.ts",
+                }),
+            ],
+        });
     },
 };
