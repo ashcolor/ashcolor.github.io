95b1bdc1d95c6613f2c2367d83b8fbdb74a5b6c0
ashcolor <ashcolor27@gmail.com>
2022-10-17 22:41:30 +0900

    chg: スクロールバーデザイン

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,16 @@
     <MyFooter />
 </template>
 
-<style scoped></style>
+<style>
+::-webkit-scrollbar {
+    width: 12px; /* 横幅 */
+}
+
+::-webkit-scrollbar-thumb {
+    background: #e4e4e7; /* ハンドルの色 */
+}
+
+::-webkit-scrollbar-track {
+    background: #00000000; /* 背景色 */
+}
+</style>
